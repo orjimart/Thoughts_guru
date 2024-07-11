64e768d6f9a9533d91f2f746ef90886ba76326d5
root <orjimart@gmail.com>
2024-07-11 02:58:24 -0700

    Thoughts_guru

--- thoughts_guru/__init__.py
+++ thoughts_guru/__init__.py
@@ -1,17 +1,14 @@
-'''
-    this the the package for the lets_post app. 
-'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = '36e792fa1feb235a2ebd2c5724eaf51b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from thoughts_guru import routes
+from app import routes
